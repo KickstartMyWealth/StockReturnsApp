892eafdf6c2de6ea997470316f683821c1c35351
KickstartMyWealth <nilaphone@gmail.com>
2026-07-18 20:43:24 +0700

    Update fetch-data.mjs

--- fetch-data.mjs
+++ fetch-data.mjs
@@ -12,7 +12,8 @@ const UNIQUE_TICKERS = [
   "XLK","XLF","XLE","XLV","XLY","XLP","XLI","XLU","XLB","XLRE",
   "SCHB","SCHX","SCHG","SCHV","SCHM","SCHA","SCHH","SCHF","SCHC","SCHE","SCHP","SCHO","SCHR",
   "VTI","VV","VUG","VTV","VO","VB","VNQ","VEA","VSS","VWO",
-  "IWV","IVV","IVW","IVE","IJH","IJR","IYR","EFA","SCZ","EEM","TIP","SHY","IEI"
+  "IWV","IVV","IVW","IVE","IJH","IJR","IYR","EFA","SCZ","EEM","TIP","SHY","IEI",
+  "QQQ","TOPT"
 ];
 
 const UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0 Safari/537.36";
